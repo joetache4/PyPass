ecccb48ca6315aa6b02b025e4f3600e8c8827937
Joe Tacheron <joetache4@gmail.com>
2020-08-01 13:09:14 -0400

    more tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -62,7 +62,7 @@ class TestParser:
 				p.parse("add b/b -g --no-clip")
 				
 			with pytest.raises(ValueError):
-				p.parse("add b/b/b -g --no-clip")
+				p.parse("add b\\b/b -g --no-clip")
 				
 			with pytest.raises(ValueError):
 				p.parse("add .c -g --no-clip")
@@ -90,9 +90,41 @@ class TestParser:
 			assert not any(f.startswith("a.") for f in os.listdir(".backup"))
 			assert os.path.isfile("b")
 			assert any(f.startswith("b.") for f in os.listdir(".backup"))
+			
+			p.parse("mv b a/a")
+			assert not os.path.isfile("b")
+			assert not any(f.startswith("b.") for f in os.listdir(".backup"))
+			assert os.path.isfile("a")
+			assert any(f.startswith("a.") for f in os.listdir(".backup/a"))
+			
+			p.parse("mv a b\\b") # autocompletes a
+			assert not os.path.isdir("a")
+			assert not os.path.isdir(".backup/a")
+			assert os.path.isfile("b/b")
+			assert any(f.startswith("b.") for f in os.listdir(".backup/b"))
+			
+			p.parse("mv b/b a/a/a")
+			assert not os.path.isdir("b")
+			assert not os.path.isdir(".backup/b")
+			assert os.path.isfile("a/a/a")
+			assert any(f.startswith("a.") for f in os.listdir(".backup/a/a"))
 	
 	def test_rm(self):
-		pass
+		p = pypass.Parser("password")
+		
+		monkeypatch.setattr(pypass.Parser, "_clip_text", lambda: pytest.fail("Clipboard disabled"))
+		
+		with helpers.replace_stdin(["y"]):
+			p.parse("add aa -g --no-clip")
+			p.parse("rm a")
+			assert not os.path.isfile("aa")
+			assert any(f.startswith("aa.") for f in os.listdir(".backup"))
+		
+		with helpers.replace_stdin():
+			p.parse("add aa/a -g --no-clip")
+			p.parse("rm a")
+			assert not os.path.isdir("aa")
+			assert any(f.startswith("a.") for f in os.listdir("aa/.backup"))
 	
 	def test_ls(self, capsys, helpers):
 		p = pypass.Parser("password")
@@ -111,14 +143,57 @@ class TestParser:
 		captured = capsys.readouterr()
 		assert captured.out == helpers.lines_str(["", "Accounts:", "  aaa", "  new account", ""])
 	
-	def test_print_account(self):
+	def test_print_account(self, helpers, capsys, monkeypatch):
 		pass
 	
 	def test_edit(self):
 		pass
 	
 	def test_load(self):
-		pass
+		sample = """
+cc pin
+083
+
+gmail
+password123
+pinkroses
+
+AOL
+pw
+user: dudeguy
+
+amazon
+pw
+dudeguy
+
+laptop
+1234
+
+netflix
+cat33
+dog33@gmail.com
+
+geico
+$2011$
+pinkroses@aol.com
+
+"""
 	
-	def test_copy(self):
-		pass
+	def test_copy(self, helpers, monkeypatch):
+		p = pypass.Parser("master")
+		
+		with helpers.replace_stdin(["passwd123"]):
+			p.parse("add abc --no-clip")
+		clip = helpers.ShellFunc()
+		monkeypatch.setattr(pypass.Parser, "_clip_text", clip.call)
+		p.parse("ab")
+		assert clip.called
+		assert clip.args[0] == "passwd123"		
+		
+		with helpers.replace_stdin(["passwd_xyz", "someusername", "otherinfo"]):
+			p.parse("add xyz -m --no-clip")
+		clip = helpers.ShellFunc()
+		monkeypatch.setattr(pypass.Parser, "_clip_text", clip.call)
+		p.parse("copy x")
+		assert clip.called
+		assert clip.args[0] == "passwd_xyz"
